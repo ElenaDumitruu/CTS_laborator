fbe3af6587ae3efb804f325f94f6b3c40f978fe1
elena <elena@LAPTOP-CUPSEFN1>
2021-02-25 13:20:32 +0200

    Cod aranjat

--- HelloWorld/src/ro/ase/csie/cts/laborator/TestHello.java
+++ HelloWorld/src/ro/ase/csie/cts/laborator/TestHello.java
@@ -5,8 +5,8 @@ public class TestHello {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world!");
- System.out.println("sunt frm");
- System.out.println("casa");
+		System.out.println("sunt frm");
+		System.out.println("casa");
 		
 	}
 
